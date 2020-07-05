b429630e63b1c0b8bbf2c41a0cb6e44de6bb8fe1
prakash0705 <prakashkumaran78@gmail.com>
2020-07-05 09:54:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder,Validators, EmailValidator} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyApp';
+  userForm;
+  userDetails=[];
+  constructor(private fb:FormBuilder)
+  {
+    this.userForm=this.fb.group({
+      'name':this.fb.control('',[Validators.required,Validators.minLength(5),Validators.maxLength(20)]),
+      'email':this.fb.control('',Validators.required),
+      'dob':this.fb.control('',Validators.required),
+      'country':this.fb.control('',Validators.required),
+      'gender':this.fb.control('',Validators.required),
+      'favfood':this.fb.control('',Validators.required),
+      'address':this.fb.array([
+        this.fb.group({
+        'street':this.fb.control(''),
+        'door':this.fb.control(''),
+        'pin':this.fb.control('')
+        }),
+        this.fb.group({
+          'street':this.fb.control(''),
+          'door':this.fb.control(''),
+          'pin':this.fb.control('')
+          })
+      ])
+    })
+  }
+submitForm()
+{
+  if(this.userForm.valid)
+  {
+    console.log(this.userForm.value);
+    this.userDetails.push(this.userForm.value);
+  }
+}
+
+
+
+
+
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
-import { PriceTagComponent } from './price-tag/price-tag.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PriceTagComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
